f27b48fd7e7e7429b31d5c6d6133263ec6ec0cc1
NorthVaryag <olegklimuk2008@gmail.com>
2026-05-18 11:58:27 +1000

    .

--- CourseProject1125/ViewModels/MainWindowViewModel.cs
+++ CourseProject1125/ViewModels/MainWindowViewModel.cs
@@ -3,6 +3,8 @@ using System.Data;
 using Microsoft.Data.SqlClient;
 using System.ComponentModel;
 using System.Runtime.CompilerServices;
+using Avalonia.Controls;
+using Avalonia.Interactivity;
 using CourseProject1125.DB;
 using Tmds.DBus.Protocol;
 using Microsoft.Extensions.DependencyInjection;
@@ -10,35 +12,8 @@ using Connection = CourseProject1125.DB.Connection;
 
 namespace CourseProject1125.ViewModels
 {
-    public class MainWindowViewModel : INotifyPropertyChanged
+    public partial class MainWindowViewModel
     {
-       
 
-        public MainWindowViewModel()
-        {
-            LoadData();
-        }
-
-        private void LoadData()
-        {
-            try
-            {
-               
-                string connectionString = Connection.ConnectionString; 
-
-               
-            }
-            catch (Exception ex)
-            {
-                Console.WriteLine(ex.Message);
-            }
-        }
-
-       
-        public event PropertyChangedEventHandler PropertyChanged;
-        public void OnPropertyChanged([CallerMemberName] string prop = "")
-        {
-            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(prop));
-        }
     }
 }
